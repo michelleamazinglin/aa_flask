58384c01fe8ee41f05273076be01677e1460336d
Michelle Lin <michelleamazinglin@gmail.com>
2022-05-17 01:33:36 -0400

    learning about flask route

--- simple.py
+++ simple.py
@@ -2,6 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return '<h1>Hello, world!</h1>'
+@app.route('/home')
+def home():
+    return '<h1>Home</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About</h1>'
+
+
+@app.route('/item/<int:id>')
+def item(id):
+    return f'<h1>Item {id}</h1>'
+
+
+@app.before_request
+def before_request_function():
+    print("before_request is running")
+
+
+@app.before_first_request
+def before_first_function():
+    print("before_first_request happens once")
