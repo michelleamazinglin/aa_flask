daca8fad84217cbbe8bef04c5fa54d99d498cd95
Michelle Lin <michelleamazinglin@gmail.com>
2022-06-03 12:56:30 -0400

    jinja template with flask app

--- simple.py
+++ simple.py
@@ -1,29 +1,8 @@
-from flask import Flask
+from flask import (Flask, render_template)
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return '<h1>Home</h1>'
-
-
-@app.route('/about')
-def about():
-    return '<h1>About</h1>'
-
-
-@app.route('/item/<int:id>')
-def item(id):
-    return f'<h1>Item {id}</h1>'
-
-
-@app.before_request
-def before_request_function():
-    print("before_request is running")
-
-
-@app.before_first_request
-def before_first_function():
-    print("before_first_request happens once")
+def index():
+    return render_template('index.html', sitename="Michelle's site", page="home")
